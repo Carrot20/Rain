0a153df8f206b09258fa954ac1bf22d5460b09c9
Ritsu95 <ritsu.chan.95@gmail.com>
2014-01-23 10:38:58 +0100

    Episode 7
    Buffering images and added rasters.

--- src/com/ritsu/rain/Game.java
+++ src/com/ritsu/rain/Game.java
@@ -5,12 +5,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
@@ -19,10 +20,12 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	private boolean running = false;
 
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		
+
 		frame = new JFrame();
 	}
 
@@ -47,17 +50,17 @@ public class Game extends Canvas implements Runnable {
 			render();
 		}
 	}
-	
+
 	public void update() {
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
 		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
@@ -74,7 +77,7 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.frame.setLocationRelativeTo(null);
 		game.frame.setVisible(true);
-		
+
 		game.start();
 	}
 
