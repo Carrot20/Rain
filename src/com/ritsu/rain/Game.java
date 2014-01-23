081cb1b22bfc9d4bd05894369c9c49188004736a
Ritsu95 <ritsu.chan.95@gmail.com>
2014-01-23 09:59:06 +0100

    Episode 4
    Window created.

--- src/com/ritsu/rain/Game.java
+++ src/com/ritsu/rain/Game.java
@@ -1,20 +1,34 @@
 package com.ritsu.rain;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
-	
+
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+
 	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -23,11 +37,24 @@ public class Game implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		while (running) {
-			
+			System.out.println("Running.");
 		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 
 }
