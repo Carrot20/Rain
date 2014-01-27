6abd5826bc6073fef5f7d06fd51f7794fa39b346
Ritsu95 <ritsu.chan.95@gmail.com>
2014-01-27 12:13:25 +0100

    Episode 18
    Added map controlling with wasd and arrows.

--- src/com/ritsu/rain/input/Keyboard.java
+++ src/com/ritsu/rain/input/Keyboard.java
@@ -6,28 +6,25 @@ import java.awt.event.KeyListener;
 public class Keyboard implements KeyListener {
 
 	private boolean[] keys = new boolean[120];
-	public boolean up, down, left, rigth;
-	
+	public boolean up, down, left, right;
+
 	public void update() {
 		up = keys[KeyEvent.VK_UP] || keys[KeyEvent.VK_W];
-		up = keys[KeyEvent.VK_DOWN] || keys[KeyEvent.VK_S];
-		up = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
-		up = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
-		
-		System.out.println(up);
+		down = keys[KeyEvent.VK_DOWN] || keys[KeyEvent.VK_S];
+		left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
+		right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
 	}
-	
 
 	public void keyPressed(KeyEvent e) {
 		keys[e.getKeyCode()] = true;
 	}
 
 	public void keyReleased(KeyEvent e) {
-		keys[e.getKeyCode()] = true;
+		keys[e.getKeyCode()] = false;
 	}
 
 	public void keyTyped(KeyEvent e) {
-		
+
 	}
 
 }
