e33810c6ff210d4dff312faf78503e235362e5f4
Ritsu95 <ritsu.chan.95@gmail.com>
2014-01-27 12:30:01 +0100

    Episode 19
    Created SpritheSheet class.

--- src/com/ritsu/rain/graphics/Screen.java
+++ src/com/ritsu/rain/graphics/Screen.java
@@ -6,8 +6,8 @@ public class Screen {
 
 	private int width, height;
 	public int[] pixels;
-	public final int MAP_SIZE = 8;
-	public final int MAP_SIZE_MASK = 7;
+	public final int MAP_SIZE = 64;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 
