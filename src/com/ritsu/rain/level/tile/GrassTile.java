585b8cbf57491bc58c45b3f2c8faaea254dba6c2
Ritsu95 <ritsu.chan.95@gmail.com>
2014-02-12 12:24:12 +0100

    Episode 27

---
+++ src/com/ritsu/rain/level/tile/GrassTile.java
@@ -0,0 +1,16 @@
+package com.ritsu.rain.level.tile;
+
+import com.ritsu.rain.graphics.Screen;
+import com.ritsu.rain.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		// Do render stuff here!
+	}
+
+}
