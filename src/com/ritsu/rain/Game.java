c9928c84f472c8068368d33a9bd29340a6605944
Ritsu95 <ritsu.chan.95@gmail.com>
2014-03-05 10:35:33 +0100

    Episode 76

--- src/com/ritsu/rain/Game.java
+++ src/com/ritsu/rain/Game.java
@@ -131,15 +131,6 @@ public class Game extends Canvas implements Runnable {
 		int yScroll = player.y - screen.height / 2;
 		level.render(xScroll, yScroll, screen);
 		player.render(screen);
-
-		Sprite sprite = new Sprite(2, 2, 0xffffff);
-		Random random = new Random();
-		for (int i = 0; i < 100; i++) {
-			int x = random.nextInt(20);
-			int y = random.nextInt(20);
-			screen.renderSprite(width - 60, 50, sprite, true);
-		}
-
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
